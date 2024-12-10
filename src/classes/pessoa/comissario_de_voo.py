5d010d47da6ffa350c993306b3807f87816a4f5e
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2024-12-10 14:02:13 -0300

    Implementa cadastro de comissario

--- src/classes/pessoa/comissario_de_voo.py
+++ src/classes/pessoa/comissario_de_voo.py
@@ -1,7 +1,69 @@
-from pessoa import Pessoa
+import pandas as pd
+from .pessoa import Pessoa
+import funcionalidades.arquivos.manipula_arquivos as ManipularArquivos
+
+CAMINHO_COMISSARIOS = "base_dados/tripulacao/comissarios.json"
+cabecalho = 'Comissarios'
+colunas = ["nome","cpf","linguas","numero_certificado"]
+
 
 class ComissarioDeVoo(Pessoa):
-    def __init__(self, nome, cpf, linguas, numero_certificado) -> None:
+    def __init__(self, nome, cpf) -> None:
         super().__init__(nome, cpf)
-        self.__linguas = linguas
-        self.__numero_certificado = numero_certificado
+        self.__linguas = []
+        self.__numero_certificado = None
+
+    #--------------GET--------------
+
+    @property
+    def linguas(self):
+        return self.__linguas
+    
+    @property
+    def numero_certificado(self):
+        return self.__numero_certificado
+
+    #--------------SET--------------
+
+    @linguas.setter
+    def linguas(self, valor):
+        lista_linguas = ['Português', 'Inglês', 'Alemão', 'Russo', 'Chinês', 'Japonês', 'Francês', 'Italiano']
+   
+        for lingua in valor:
+            if lingua not in lista_linguas:  
+                raise ValueError(f"Língua inválida: {lingua}")
+        self.__linguas = valor
+    
+    @numero_certificado.setter
+    def numero_certificado(self, valor):
+        if valor.isdigit():  
+            self.__numero_certificado = valor
+        else:
+            raise ValueError("Número do Certificado inválido!")
+
+    #--------------PRIVATE--------------
+
+    #--------------PUBLIC---------------
+
+    def cadastrar(nome, cpf, linguas, numero_certificado):
+        comisario = ComissarioDeVoo(nome, cpf)
+        comisario.linguas = linguas
+        comisario.numero_certificado = numero_certificado
+
+        df = ComissarioDeVoo.carregarListaComissarios()
+        novo_comissario = pd.DataFrame([
+            {
+                "nome": comisario.nome,
+                "cpf": comisario.cpf,
+                "linguas": comisario.linguas,
+                "numero_certificado": comisario.numero_certificado
+            }
+        ])
+
+        df = pd.concat([df, novo_comissario], ignore_index=True)
+        ManipularArquivos.salvar_informacoes(df, CAMINHO_COMISSARIOS, cabecalho)
+    
+    def carregarListaComissarios():
+        lista_comissarios = ManipularArquivos.carregar_informacoes(CAMINHO_COMISSARIOS, cabecalho, colunas)
+        return lista_comissarios
+    
