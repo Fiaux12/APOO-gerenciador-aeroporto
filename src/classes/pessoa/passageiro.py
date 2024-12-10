12978431662593295a3efb09343dd3c6340f11a1
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2024-12-10 20:07:54 -0300

    cria dados passageiros

--- src/classes/pessoa/passageiro.py
+++ src/classes/pessoa/passageiro.py
@@ -1,6 +1,19 @@
 from pessoa import Pessoa
 
 class Passageiro(Pessoa):
-    def __init__(self, nome, cpf, idVoo) -> None:
+    def __init__(self, nome:str, cpf:int) -> None:
         super().__init__(nome, cpf)
-        self.__idVoo = idVoo
+        self.__passaporte = None 
+
+   #--------------GET--------------
+
+    @property
+    def passaporte(self):
+        return self.__passaporte
+
+    #--------------SET--------------
+
+    @passaporte.setter
+    def latitude(self, valor):
+        self.__passaporte = valor
+   
